7943b8c2446aaff076fa05d06b1dcb35ee7c659f
Yazir <alexishernandezgt05@gmail.com>
2025-11-06 01:07:30 -0600

    mig 6 a pg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   getWelcome() {
     return {
       message: '¡Bienvenido al Backend! ',
