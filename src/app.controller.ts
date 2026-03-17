f5bbad4de1253c5cd4fc3b7b266956e74d2d14fa
Yazir <alexishernandezgt05@gmail.com>
2026-03-16 19:33:27 -0600

    fix: app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  getHello(): any {
-    throw new Error('Method not implemented.');
-  }
   @Get()
   getWelcome() {
     return {
-      message: 'Backend corriendo! ',
+      message: 'Backend corriendo!',
       status: 'online',
       timestamp: new Date().toISOString(),
     };
